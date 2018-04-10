79c8644f2fd3da4327c0d41b961900ef1b1953d8
Asutosh Sahoo <asutosha.sahoo1@centurylink.com>
2018-04-10 19:49:40 +0530

    updated

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,4 @@
-
+import java.util.Date;
 public class HelloWorld {
 
 	public String hello() {
@@ -7,7 +7,8 @@ public class HelloWorld {
 
 	}
 	public static void main(String args[]) {
-		System.out.println("Hello World1");
+		System.out.println("Hello World !");
+		System.out.println("Current time : "+new Date());
 
 	}
 
