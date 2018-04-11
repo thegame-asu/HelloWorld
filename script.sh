4ff4f8b01a9374c58b8895651e5441ca0bd648b5
Asutosh Sahoo <asutosha.sahoo1@centurylink.com>
2018-04-11 13:35:47 +0530

    updated

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@
 
 
 echo "compiling..."
-javac -d "classes" TestHelloWorld.java
+javac -d "bin" "src/TestHelloWorld.java"
 echo "running the program..."
-java -classpath "src" TestHelloWorld
+java -classpath "bin" TestHelloWorld
 
 sleep 5
